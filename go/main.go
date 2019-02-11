fa1e10ad3ba37b1327c9cd8fa209d241d0b8c976
Divan Visagie <me@dvisagie.com>
2019-02-11 16:43:37 +0200

    Finish go

--- go/main.go
+++ go/main.go
@@ -6,31 +6,48 @@ import (
 	"strconv"
 )
 
-func convertStringsToNumbers(strings []string) []int {
+func processDelimiter(input string) (string, rune) {
+	hasDelim, _ := regexp.MatchString("//.*", input)
+	if !hasDelim {
+		return input, ','
+	}
+	splitRegex := regexp.MustCompile("^//|\n")
+	split := splitRegex.Split(input, -1)
+	delim := split[1]
+	return split[2], []rune(delim)[0]
+}
+
+func convertStringsToNumbers(strings []string) ([]int, error) {
 	var ints = []int{}
 	for _, s := range strings {
 		n, _ := strconv.Atoi(s)
+		if n < 0 {
+			return nil, fmt.Errorf("negatives not allowed: %d", n)
+		}
 		ints = append(ints, n)
 	}
-	return ints
+	return ints, nil
 }
 
-func add(input string) int {
+func add(input string) (int, error) {
 	if input == "" {
-		return 0
+		return 0, nil
 	}
+	cleanInput, delim := processDelimiter(input)
+	splitRegex := regexp.MustCompile(fmt.Sprintf("%c|\n", delim))
 
-	splitRegex := regexp.MustCompile(",|\n")
-
-	numbersStrings := splitRegex.Split(input, -1)
-	numbers := convertStringsToNumbers(numbersStrings)
+	numbersStrings := splitRegex.Split(cleanInput, -1)
+	numbers, err := convertStringsToNumbers(numbersStrings)
+	if err != nil {
+		return 0, err
+	}
 
 	var sum = 0
 	for _, element := range numbers {
 		sum += element
 	}
 
-	return sum
+	return sum, nil
 }
 
 func main() {
--- go/main_test.go
+++ go/main_test.go
@@ -1,16 +1,20 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_add(t *testing.T) {
 	type args struct {
 		input string
 	}
-	tests := []struct {
+	type TestParams struct {
 		name string
 		args args
 		want int
-	}{
+		err  string
+	}
+	tests := []TestParams{
 		{
 			name: "given empty string",
 			args: args{""},
@@ -36,11 +40,67 @@ func Test_add(t *testing.T) {
 			args: args{"1\n2,3"},
 			want: 6,
 		},
+		{
+			name: "given //;\n1;2",
+			args: args{"//;\n1;2"},
+			want: 3,
+		},
+		{
+			name: "given nevative number",
+			args: args{"1,4,-1"},
+			want: 0,
+			err:  "negatives not allowed: -1",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := add(tt.args.input)
+
+			if got != tt.want {
+				t.Errorf("add() = %v, want %v", got, tt.want)
+			}
+
+			if err != nil {
+				if err.Error() != tt.err {
+					t.Errorf("add() error = %v, want %v", err.Error(), tt.err)
+				}
+			}
+		})
+	}
+}
+
+func Test_processDelimiter(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name           string
+		args           args
+		wantCleanInput string
+		wantDelim      rune
+	}{
+		{
+			name:           "given no delimiter in string",
+			args:           args{"1,2,3"},
+			wantCleanInput: "1,2,3",
+			wantDelim:      ',',
+		},
+		{
+			name:           "given delimiter in string",
+			args:           args{"//:\n1:2:3"},
+			wantCleanInput: "1:2:3",
+			wantDelim:      ':',
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := add(tt.args.input); got != tt.want {
-				t.Errorf("add() = %v, want %v", got, tt.want)
+			got, got1 := processDelimiter(tt.args.input)
+			if got != tt.wantCleanInput {
+				t.Errorf("processDelimiter() gotCleanInput = %v, want %v", got, tt.wantCleanInput)
+			}
+			if got1 != tt.wantDelim {
+				t.Errorf("processDelimiter() gotDelim = %v, want %v", got1, tt.wantDelim)
 			}
 		})
 	}
